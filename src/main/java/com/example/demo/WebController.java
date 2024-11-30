954c450a2b1232aecfd1150e8e43c958c30c60a5
sahil7879 <sahil1234uwrrt@gmail.com>
2024-11-30 09:48:16 +0530

    first commit

---
+++ src/main/java/com/example/demo/WebController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Welcome to My Simple Java Web App!</h1>";
+    }
+}
